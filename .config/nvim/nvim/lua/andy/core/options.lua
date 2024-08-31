e5eff95be0f8f879ccbac52d2c8b77c24c78d158
andy4747 <angeldhakal97@gmail.com>
2024-08-31 19:39:32 -0400

    dotfiles

---
+++ .config/nvim/nvim/lua/andy/core/options.lua
@@ -0,0 +1,26 @@
+vim.cmd("let g:netrw_liststyle = 3")
+
+local opt = vim.opt
+
+opt.relativenumber = true
+opt.number = true
+
+-- tabs & indentation
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+opt.autoindent = true
+
+-- search settings
+opt.ignorecase = true
+opt.smartcase = true
+
+-- backspace
+opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
+
+-- clipboard
+opt.clipboard:append("unnamedplus") -- use system clipboard as default register
+
+-- turn off swapfile
+opt.swapfile = false
