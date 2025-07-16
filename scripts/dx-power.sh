7f71c3c0f297316a1da08318d3ac3f9ed52b32f5
angel-dx <angeldhakal97@gmail.com>
2025-07-16 15:17:11 -0400

    rofi: power

--- scripts/dx-power.sh
+++ scripts/dx-power.sh
@@ -1,57 +1,64 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-# Rofi Power Menu Script - Robust, Modular
-# Place this in ~/.config/rofi/powermenu.sh and make it executable
+# Rofi Power Menu Script
+# Save as: ~/dotfiles/scripts/dx-power.sh
+# Make executable: chmod +x ~/dotfiles/scripts/dx-power.sh
 
-# Theme (optional, can be customized)
-rofi_cmd="rofi -theme-str 'window { width: 20%; }' -dmenu -i -p 'Power Menu 🧠'"
+# Icons (using Nerd Font icons)
+shutdown_icon="⏻"
+reboot_icon="󰜉"
+logout_icon="󰍃"
+hibernate_icon="󰤄"
+cancel_icon="󰜺"
 
-# Power menu options
-options=(
-  "  Lock"
-  "  Suspend"
-  "⏾  Hibernate"
-  "  Reboot"
-  "⏻  Poweroff"
-  "  Logout"
-  "󰜺  Cancel"
-)
+# Options
+options="$logout_icon Logout\n$hibernate_icon Hibernate\n$reboot_icon Reboot\n$shutdown_icon Shutdown\n$cancel_icon Cancel"
 
-# Convert array to newline-separated string for Rofi
-choice=$(printf "%s\n" "${options[@]}" | $rofi_cmd)
+chosen="$(echo -e "$options" | rofi -dmenu -i -p "Power Menu" -matching fuzzy -theme ~/.config/rofi/themes/dx-power.rasi)"
 
-# Confirmation prompt
-confirm() {
-  rofi -dmenu -i -p "Are you sure? (y/n)" <<< ""
-}
-
-# Commands
-case "$choice" in
-  *Lock*)
-    if command -v i3lock >/dev/null; then
-      i3lock
-    elif command -v betterlockscreen >/dev/null; then
-      betterlockscreen -l
-    else
-      notify-send "Lock command not found"
-    fi
-    ;;
-  *Suspend*)
-    confirm | grep -qi "^y" && systemctl suspend
-    ;;
-  *Hibernate*)
-    confirm | grep -qi "^y" && systemctl hibernate
-    ;;
-  *Reboot*)
-    confirm | grep -qi "^y" && systemctl reboot
-    ;;
-  *Poweroff*)
-    confirm | grep -qi "^y" && systemctl poweroff
-    ;;
-  *Logout*)
-    confirm | grep -qi "^y" && qtile cmd-obj -o cmd -f shutdown
-    ;;
-  *Cancel*|*)
-    exit 0
-    ;;
+# Execute based on choice
+case $chosen in
+    "$shutdown_icon Shutdown")
+        # Confirmation dialog
+        confirm=$(echo -e "Yes\nNo" | rofi -dmenu -i -p "Shutdown system?" -theme ~/.config/rofi/themes/dx-power.rasi)
+        if [[ $confirm == "Yes" ]]; then
+            systemctl poweroff
+        fi
+        ;;
+    "$reboot_icon Reboot")
+        # Confirmation dialog
+        confirm=$(echo -e "Yes\nNo" | rofi -dmenu -i -p "Reboot system?" -theme ~/.config/rofi/themes/dx-power.rasi)
+        if [[ $confirm == "Yes" ]]; then
+            systemctl reboot
+        fi
+        ;;
+    "$logout_icon Logout")
+        # Confirmation dialog
+        confirm=$(echo -e "Yes\nNo" | rofi -dmenu -i -p "Logout from session?" -theme ~/.config/rofi/themes/dx-power.rasi)
+        if [[ $confirm == "Yes" ]]; then
+            # For qtile
+            qtile cmd-obj -o cmd -f shutdown
+            # Alternative methods (uncomment the one that works for your setup):
+            # loginctl terminate-session ${XDG_SESSION_ID-}
+            # pkill -KILL -u "$USER"
+        fi
+        ;;
+    "$hibernate_icon Hibernate")
+        # Optional confirmation
+        confirm=$(echo -e "Yes\nNo" | rofi -dmenu -i -p "Hibernate system?" -theme ~/.config/rofi/themes/dx-power.rasi)
+        if [[ $confirm == "Yes" ]]; then
+            # Lock first (optional)
+            if command -v i3lock &> /dev/null; then
+                i3lock -c 000000 -f &
+            fi
+            systemctl hibernate
+        fi
+        ;;
+    "$cancel_icon Cancel")
+        exit 0
+        ;;
+    *)
+        # If nothing is selected or ESC is pressed
+        exit 0
+        ;;
 esac
--- scripts/launch.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-choosen=$(ls /home/angeldx/dotfiles/scripts/dmenu/ | dmenu -i)
-[ -n "$choosen" ] && exec /home/angeldx/dotfiles/scripts/dmenu/"$choosen"
